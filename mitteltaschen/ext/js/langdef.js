9431fbd27f531a69a147c2943f8970a249b59260
BeSoBAG4u <BeSoBAG4u@gmail.com>
2014-05-04 18:13:25 +0200

    Fotos medium Taschen

---
+++ mitteltaschen/ext/js/langdef.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////
+//
+// Porta text resources
+//
+// Language (in English) : German
+//
+// ISO 3166 country code : DE
+//
+// Translation by        : Kenneth Weidlich
+//
+////////////////////////////////////////////////
+var ls_previous   = 'Zurück';
+var ls_next       = 'Weiter';
+var ls_home       = 'Anfang';
+var ls_showall    = 'Komplett';
+var ls_lightbox   = 'Lichttisch';
+var ls_albumview  = 'Zum Album';
